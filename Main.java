7b0b24405f42bd59313f1a8513fef3f8ed9f5d1f
marsh-sb <enotaku5526@gmail.com>
2021-03-29 21:08:44 +0900

    update Main.java

--- Main.java
+++ Main.java
@@ -98,8 +98,88 @@ class Main2 {
   }
 }
 
+class Boolean {
+  public static void main(String[] args) {
+    // 「true」を用いて、「真」を表す真偽値を出力してください
+    System.out.println(true);
+    
+    // 「false」を用いて、「偽」を表す真偽値を出力してください
+    System.out.println(false);
+    
+    // 「==」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 == 3);
+    
+    // 「!=」を用いて、値を比較した結果を出力してください
+    System.out.println(12 / 4 != 3);
+    
+    // 変数を定義し、値を比較した結果を代入してください
+    boolean bool = 3 * 9 == 27;
+    
+    // 変数boolの値を出力してください
+    System.out.println(bool);
+    
+  }
+}
+
 class Main3 {
   public static void main(String[] args) {
-    System.out.println("Hello Java");
+    // trueと出力されるようにしてください
+    System.out.println(true || false);
+    
+    // falseと出力されるようにしてください
+    System.out.println(false && true);
+    
+    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 && 3 >= 2);
+    
+    // 「8 < 5」または「3 >= 2」の結果を出力してください
+    System.out.println(8 < 5 || 3 >= 2);
+    
+    // 「8 < 5」でない、の結果を出力してください
+    System.out.println(!(8 < 5));
+    
+  }
+}
+
+class Main4 {
+  public static void main(String[] args) {
+    // 条件式に直接trueをいれてください
+    if (true) {
+      System.out.println("条件式がtrueのため、出力されます");
+    }
+    
+    // 条件式に直接falseをいれてください
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されません");
+    }
+    
+    int x = 5;
+    // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力してください
+    if (x > 2){
+      System.out.println("xは2より大きい");
+    }
+    
+    // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力してください
+    if (x > 8){
+      System.out.println("xは8より大きい");
+    }
+    
+  }
+}
+
+class Main5 {
+  public static void main(String[] args) {
+    int number = 12;
+    
+    // numberが20より小さいとき、どちらでもないときの条件分岐を追加してください
+    if (number < 10) {
+      System.out.println("10より小さい");
+    } 
+    else if (number < 20){
+      System.out.println("10以上、20より小さい");
+    }
+    else {
+      System.out.println("20以上");
+    }
   }
 }
